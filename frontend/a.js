141f461a35339538eff5071c2f04f55d874f7649
duzenko <anton.duzenko@gmail.com>
2019-03-19 18:50:21 +0200

    Basic info

--- frontend/a.js
+++ frontend/a.js
@@ -7,7 +7,8 @@ function render(now) {
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
     gl.viewport(0, 0, canvas.width, canvas.height)
-    
+    document.getElementById('viewport').innerHTML = canvas.width + 'x' + canvas.height
+
     vp = gl.getParameter(gl.VIEWPORT);
     // Set clear color to black, fully opaque
     gl.clearColor(0.5, now % 1000 * 0.001, 0.0, 1.0);
